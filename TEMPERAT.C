b6d39f97241b4369cda6f21db44ac211a672259b
sidhant112233 <137783068+sidhant112233@users.noreply.github.com>
2023-07-08 22:50:47 +0530

    Add files via upload

---
+++ TEMPERAT.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=1,i,f=1;
+	clrscr();
+	printf("Enter value =");
+	scanf("%d",&i);
+
+	for(i;a<=i;a++);
+	f=f*a;
+	{
+	 printf("f=%d\t",f);
+	}
+	getch();
+}
